4a23e6943d126c487c7e05229ffe62cb3c658d40
flinraider <leonardo.teixeira.dev@gmail.com>
2021-11-08 23:07:32 -0300

    correção de bugs

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
-#include "sneak.hh"
+#include "snake.hh"
 
-int main(int argc, char const *argv[]) {
-
-    SneakGame s;
+int main(int argc, char *argv[]) {
+    SnakeGame s;
     s.start();
-    
     return 0;
 }
-
 // g++ main.cpp -lncurses -ltinfo
--- snake.hh
+++ snake.hh
@@ -1,26 +1,25 @@
-#ifndef SNEAK_GAME_H
-#define SNEAK_GAME_H
+#ifndef SNAKE_GAME_H
+#define SNAKE_GAME_H
 #include <iostream>  // TIME E RANGE     
 #include <ncurses.h> // PARA AS FUNÇÕES 
 #include <vector>    // PARA LOGICA DAS COORDENADAS
 #include <unistd.h>
-
 // Logica para armazenar as coordenadas
 // struct
-struct SneakType {
+struct SnakeType {
    int s_x, s_y; 
-   SneakType(int, int);
-   SneakType();
+   SnakeType(int, int);
+   SnakeType();
 };
 // struct nao precisa de destrutor
 
-class SneakGame {
+class SnakeGame {
     protected:
         int m_maxwidth;  // tamanho da largura da tela
         int m_maxheight; // tamanho da altura da tela
-        std::vector<SneakType> sneak;
-        char m_sneak_char; // desenha a sneak
-        SneakType v_food;
+        std::vector<SnakeType> snake;
+        char m_snake_char; // desenha a sneak
+        SnakeType v_food;
         char m_food_char;
         int m_delay;
         char m_direction;
@@ -28,14 +27,13 @@ class SneakGame {
         int m_score;
     public:
     //construtor
-    SneakGame();               
+    SnakeGame();               
     //destrutor
-    ~SneakGame(); 
-
+    ~SnakeGame(); 
         // funções
-        void m_insert_food();
-        void movesneak();
-        bool collide();
-        void start();            
+       void m_insert_food();
+       void movesnake();
+       bool _collide();
+       void start();            
 };
 #endif
